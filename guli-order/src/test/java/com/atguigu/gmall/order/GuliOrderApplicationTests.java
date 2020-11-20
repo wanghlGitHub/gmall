1de13e42b97ba6963997a74efe2715f337cd2ec9
git <wangheliang@bj-sjzy.com>
2020-11-20 13:02:17 +0800

    测试类修改

--- guli-order/src/test/java/com/atguigu/gmall/order/GuliOrderApplicationTests.java
+++ guli-order/src/test/java/com/atguigu/gmall/order/GuliOrderApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class GuliOrderApplicationTests {
+public class GuliOrderApplicationTests {
 
 	@Test
 	void contextLoads() {
