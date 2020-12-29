58d59698f81f5e1de40201ded01c1bc9367c0cda
git <wangheliang@bj-sjzy.com>
2020-12-29 15:23:26 +0800

    add cart module

---
+++ guli-cart/src/test/java/com/atguigu/gmall/cart/GuliCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.cart;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GuliCartApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
